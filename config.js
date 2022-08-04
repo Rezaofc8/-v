ad47d4e762de5ca85863560f97803c9eab5a12b9
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-08-04 21:03:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 //=============『 Utama 』================== //
-global.owner = ['6282xxxxxx'] 
-global.mods = ['6282xxxxxx'] 
-global.prems = ['6282xxxxxx'] 
+global.owner = ['6289606790112'] 
+global.mods = ['6289606790112'] 
+global.prems = ['6289606790112,6283815019820,6281352905695'] 
 
 //=============『 Info Owner 』============== //
 global.nameowner = '⫹⫺ Owner'
-global.numberowner = '6282xxxxxx'
-global.instagram = 'https://instagram.com/prm2.0'
+global.numberowner = '6289606790112'
+global.instagram = 'https://instagram.com/itz.reza_official_'
 global.github = 'https://github.com/BOTCAHX'
-global.dana = '6282xxxxxx'
-global.pulsa = '6282xxxxxx'
+global.dana = '6289606790112'
+global.pulsa = '6289606790112'
 global.gopay = ''
 
 //=============『 Info Bot 』=================//
-global.namebot = 'BOTCAHX'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'Alok Hamil-MD'
+global.gc = 'https://chat.whatsapp.com/BCgUSCxQltq8SwcpJe1tDf'
+global.web = 'https://instagram.com/itz.reza_official_' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://database.tioclkp02.repl.co/TextPro.me_162df9a5371cb2.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://database.tioclkp02.repl.co/TextPro.me_162df9a5371cb2.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/19597d615d234c5f22a87.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/19597d615d234c5f22a87.jpg' //ini juga ganti 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© RezaOfc'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ Alok hamil-md'
+global.wm3 = '© Alok Hamil-md'
 global.wm4 = 'Ⓛ = Limit Ⓟ = Premium'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -38,7 +38,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\nAlok hamil'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
